9a7fee6d96fe1b349db6fe4ec479355bf1a045bc
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-07-24 17:04:25 +0600

    Task 3 Conditionals documented

---
+++ Tasks/Day-3/conditionals.js
@@ -0,0 +1,27 @@
+(() => {
+  /**
+   * Conditional Statement
+   */
+  //   const age = 18;
+  //   if (age > 18) {
+  //     console.log("You can vote");
+  //   } else {
+  //     console.log("You can not vote");
+  //   }
+  /**
+   * Ternary Statement
+   */
+  //   console.log(age < 18 ? "You cannot vote" : "you can vote");
+  /**
+   * Logical AND (&&)
+   */
+  //   const number = 55;
+  //   const result = number > 39 && "Passed !!";
+  //   console.log(result);
+  /**
+   * Logical OR (||)
+   */
+  //   const number = 40;
+  //   const result = number > 39 || "Failed !!";
+  //   console.log(result);
+})();
