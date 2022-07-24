0b15de8ef418037a18088bb91c37d222722f9e70
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-07-24 15:36:22 +0600

    Task 3 arrow functions documented

---
+++ Tasks/Day-3/arrowFunctions.js
@@ -0,0 +1,34 @@
+(() => {
+  /**
+   * Function Statement
+   */
+  //   function multiply(num1, num2) {
+  //     return num1 * num2;
+  //   }
+  //   console.log(multiply(1, 2));
+  /**
+   * Function Expression
+   */
+  //   const sum = function (num1, num2) {
+  //     return num1 + num2;
+  //   };
+  //   console.log(sum(5, 6));
+  /**
+   * Arrow Function (Way 1)
+   */
+  //   const sum = (num1, num2) => {
+  //     return num1 + num2;
+  //   };
+  //   console.log(sum(5, 6));
+  /**
+   * Arrow Function (Way 2)
+   */
+  //   const sum = (num1, num2) => num1 + num2;
+  //   console.log(sum(5, 6));
+  /**
+   * Arrow Function (Way 3)
+   */
+  //   const evenOrOdd = (num) => num % 2 == 0 ? num + " is even" : num + " is odd";
+  //   console.log(evenOrOdd(5));
+  //   console.log(evenOrOdd(6));
+})();
