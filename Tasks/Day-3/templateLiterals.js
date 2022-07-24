4aafaf0e5552778bcddbcebf6a74c381f577c90a
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-07-24 16:24:11 +0600

    Task 3 Template String / Literals documented

---
+++ Tasks/Day-3/templateLiterals.js
@@ -0,0 +1,17 @@
+(() => {
+  // const firstName = "Miraz";
+  // const lastName = "Vai";
+  // console.log(firstName + " " + lastName);
+  /**
+   * Template Literals
+   * Inside Curly Braces only 'JavaScript Expression' is allowed
+   * Statements means 'Command'
+   * Expression means 'Value'
+   */
+  // const firstName = "Miraz";
+  // const lastName = "Vai";
+  // console.log(`My Name is ${firstName} ${lastName}`);
+  /**
+   
+   */
+})();
