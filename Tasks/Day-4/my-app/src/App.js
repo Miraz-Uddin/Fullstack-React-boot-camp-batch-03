08afcc876fc625d937a39f0b652fd57ba3e29c08
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-07-25 06:29:29 +0600

    Tasks for Day-4 Completed

---
+++ Tasks/Day-4/my-app/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1>Hello New React</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
