021c255a82eb9a68db7ec53f1b3bbbb1b34be8b7
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-07-25 07:17:56 +0600

    Header, Footer Component Added

--- Tasks/Day-5/src/components/Footer.jsx
+++ Tasks/Day-5/src/components/Footer.jsx
@@ -1,5 +1,61 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  const date = new Date();
+  const year = date.getFullYear();
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="row justify-content-between">
+          <p className="col-sm-12 col-md-5">
+            Copyright &copy;{year} Miraz Uddin. All rights reserved.
+          </p>
+          <div className="social_links col-sm-12 col-md-7">
+            <a
+              className=" btn btn-info "
+              href="https://www.facebook.com/"
+              target="blank"
+            >
+              <i className="fa fa-facebook "></i>
+            </a>
+            <a
+              className="btn btn-info "
+              href="https://twitter.com/login?lang=en"
+              target="blank"
+            >
+              <i className="fa fa-twitter "></i>
+            </a>
+            <a
+              className="btn btn-info "
+              href="https://www.google.com/search?q=google+plus+&client=firefox-b-d&sxsrf=APq-WBvHeXzuwYeJgkItqOuCnMEXv8Bykw%3A1647414365850&ei=XYwxYsTSM-WjseMPt4CpyAo&ved=0ahUKEwiE9cvgiMr2AhXlUWwGHTdACqkQ4dUDCA0&uact=5&oq=google+plus+&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyBAgAEEMyBAgAEEMyBQgAEIAEMgUIABCABDIECAAQQzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQ6BwgAEEcQsAM6CggAEIAEEIcCEBQ6BggAEBYQHkoECEEYAEoECEYYAFCZBViWD2DXEWgBcAF4AIABeogBgQWSAQMyLjSYAQCgAQHIAQjAAQE&sclient=gws-wiz"
+              target="blank"
+            >
+              <i className="fa fa-google-plus "></i>
+            </a>
+            <a
+              className="btn btn-info "
+              href="https://www.linkedin.com/signup"
+              target="blank"
+            >
+              <i className="fa fa-linkedin "></i>
+            </a>
+            <a
+              className="btn btn-info "
+              href="https://www.instagram.com/accounts/login/"
+              target="blank"
+            >
+              <i className="fa fa-instagram "></i>
+            </a>
+            <a
+              className="btn btn-info "
+              href="https://www.whatsapp.com/ "
+              target="blank"
+            >
+              <i className="fa fa-whatsapp"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- Tasks/Day-5/src/components/Header.jsx
+++ Tasks/Day-5/src/components/Header.jsx
@@ -1,5 +1,64 @@
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <nav className="navbar navbar-expand-lg navbar-light bg-custom">
+              <div className="container">
+                <a className="navbar-brand" href="./index.html">
+                  <img src="./public/assets/img/logo.png" alt="logo" />
+                </a>
+                <button
+                  className="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div
+                  className="collapse navbar-collapse"
+                  id="navbarSupportedContent"
+                >
+                  <ul className="navbar-nav custom-list">
+                    <li className="nav-item">
+                      <a className="nav-link active" href="./index.html">
+                        home
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="./about.html">
+                        about
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="./service..html">
+                        service
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="./portfolio.html">
+                        portfolio
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="./contact.html">
+                        contact
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
