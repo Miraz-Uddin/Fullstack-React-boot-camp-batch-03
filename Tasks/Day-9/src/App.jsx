e0c9c7dbf5179a0dc8a24cad9c30b4c01a5ab31a
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-07-29 10:19:32 +0600

    API data Fetched

--- Tasks/Day-9/src/App.jsx
+++ Tasks/Day-9/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import Home from "./components/Home";
-import Nav from "./components/Nav";
+// import Nav from "./components/Nav";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Nav />
+      {/* <Nav /> */}
       <Home />
     </>
   );
--- Tasks/Day-9/src/components/Home.jsx
+++ Tasks/Day-9/src/components/Home.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  const fetchQuiz = async () => {
+    const res = await fetch(
+      "https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple"
+    );
+    const { results } = await res.json();
+    console.log(results);
+  };
+  return (
+    <>
+      <button onClick={fetchQuiz}>Start Quiz</button>
+    </>
+  );
 }
