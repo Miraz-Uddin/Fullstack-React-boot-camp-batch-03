b0fd546f5b93be806dd9dc2a0b452289416b46a5
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-07-29 11:16:59 +0600

    Answers Displayed for first Quiz

---
+++ Tasks/Day-9/src/components/AnswerCard.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AnswerCard({ answer }) {
+  return <li className="list-group-item">{answer}</li>;
+}
--- Tasks/Day-9/src/components/QuestionCard.jsx
+++ Tasks/Day-9/src/components/QuestionCard.jsx
@@ -1,9 +1,13 @@
 import Parser from "html-react-parser";
 import React from "react";
+import AnswerCard from "./AnswerCard";
 
 export default function QuestionCard({ quiz, index }) {
   console.log(quiz);
-  const { question } = quiz;
+  const { question, correct_answer, incorrect_answers } = quiz;
+  //   const answers = [correct_answer,...incorrect_answers]
+  const shufflingAnswers = [correct_answer, ...incorrect_answers];
+  //   const shufflingAnswers = arrayShuffle(answers)
   return (
     <>
       <div className="container">
@@ -15,9 +19,9 @@ export default function QuestionCard({ quiz, index }) {
                 <p className="card-text">{Parser(question)}</p>
               </div>
               <ul className="list-group list-group-flush">
-                <li className="list-group-item">An item</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
+                {shufflingAnswers.map((answer, index) => (
+                  <AnswerCard key={index} answer={answer} />
+                ))}
               </ul>
               <div className="card-body">
                 <a href="#" className="card-link">
