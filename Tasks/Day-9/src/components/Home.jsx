1986ed76256892a84077a6c30b7893fe9fcfae42
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-07-29 10:27:07 +0600

    API data Fetched

--- Tasks/Day-9/src/components/Home.jsx
+++ Tasks/Day-9/src/components/Home.jsx
@@ -2,15 +2,24 @@ import React from "react";
 
 export default function Home() {
   const fetchQuiz = async () => {
-    const res = await fetch(
-      "https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple"
-    );
-    const { results } = await res.json();
-    console.log(results);
+    try {
+      let response = await fetch(
+        "https://opentdbs.com/api.php?amount=5&category=18&difficulty=easy&type=multiple"
+      );
+      if (!response.ok)
+        throw new Error(`HTTP error! status: ${response.status}`);
+      const { results } = await response.json();
+      console.log(results);
+    } catch (e) {
+      console.log("Data cannot be Fetched");
+      console.log(e);
+    }
   };
   return (
     <>
-      <button onClick={fetchQuiz}>Start Quiz</button>
+      <button className="btn btn-success btn-sm" onClick={fetchQuiz}>
+        Start Quiz
+      </button>
     </>
   );
 }
