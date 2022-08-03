838d399a7f3cecd9b8f702c5495abfcbe2365f30
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-08-03 22:09:13 +0600

    Vite Default files added

---
+++ Tasks/Day-16/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Nav />
+      <Home />
+    </>
+  );
+}
+
+export default App;
