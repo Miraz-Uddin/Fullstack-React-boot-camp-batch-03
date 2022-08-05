a77ed9c4cf50bffe44693d88d171280496c487f1
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-08-05 17:08:15 +0600

    Text Input Validation For Empty Values Done

--- Tasks/Day-16/src/components/Home.jsx
+++ Tasks/Day-16/src/components/Home.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GeneralInput from "../formInputs/GeneralInput";
+import errorUpdate from "../utils/errorUpdate";
+
+const userInitialInfo = {
+  firstName: "",
+  lastName: "",
+};
+const userInitialnfoErrors = {
+  firstNameError: "",
+  lastNameError: "",
+};
 
 export default function Home() {
+  const [userInfo, setUserInfo] = useState(userInitialInfo);
+  const [userInfoErrors, setUserInfoErrors] = useState(userInitialnfoErrors);
+  const [hasErrors, setHasErrors] = useState(false);
+
+  const handleChange = (e) => {
+    const label = e.target.nextElementSibling.innerHTML;
+    const key = e.target.name;
+    const val = e.target.value;
+    setUserInfo((prev) => {
+      return {
+        ...prev,
+        [key]: val,
+      };
+    });
+    errorUpdate(userInfoErrors, key, val, label);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const checkErrors = Object.values(userInfoErrors).every(
+      (item) => item == ""
+    );
+    const checkValues = Object.values(userInfo).every((item) => item != "");
+    if (checkErrors && checkValues) console.log("Data Submitted");
+  };
+
+  const { firstName, lastName } = userInfo;
+  const { firstNameError, lastNameError } = userInfoErrors;
+
+  useEffect(() => {
+    const check = Object.values(userInfoErrors).every((item) => item != "");
+    if (!check) return setHasErrors(true);
+    return setHasErrors(false);
+  }, [hasErrors]);
+
   return (
     <>
       <div className="container">
@@ -9,13 +53,32 @@ export default function Home() {
           <div className="col-12">
             <div className="card shadow">
               <div className="card-body">
-                <form className="row g-3">
+                <form className="row g-3" onSubmit={handleSubmit}>
                   <div className="col-md-6 col-lg-4">
-                    <GeneralInput />
+                    <GeneralInput
+                      label="First Name"
+                      type="text"
+                      name="firstName"
+                      value={firstName}
+                      func={handleChange}
+                      hasErrors={hasErrors}
+                      errorVal={firstNameError}
+                    />
+                  </div>
+                  <div className="col-md-6 col-lg-4">
+                    <GeneralInput
+                      label="Last Name"
+                      type="text"
+                      name="lastName"
+                      value={lastName}
+                      func={handleChange}
+                      hasErrors={hasErrors}
+                      errorVal={lastNameError}
+                    />
                   </div>
                   <div className="col-12">
-                    <button type="submit" className="btn btn-primary">
-                      Sign in
+                    <button type="submit" className="btn btn-success">
+                      Submit
                     </button>
                   </div>
                 </form>
--- Tasks/Day-16/src/formInputs/GeneralInput.jsx
+++ Tasks/Day-16/src/formInputs/GeneralInput.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 
-export default function GeneralInput() {
+export default function GeneralInput({
+  label,
+  type,
+  name,
+  value,
+  func,
+  hasErrors,
+  errorVal,
+}) {
   return (
     <>
       <div className="form-floating mb-3">
         <input
-          type="text"
-          className="form-control valid"
-          //   className="form-control is-invalid"
           id="floatingInput"
-          placeholder="."
+          type={type}
+          className={`form-control ${errorVal && "is-invalid"}`}
+          name={name}
+          placeholder={name}
+          value={value}
+          onChange={func}
         />
-        <label htmlFor="floatingInput">First Name</label>
+        <label htmlFor="floatingInput">{label}</label>
       </div>
+      {hasErrors && <div className="invalid-feedback">{errorVal}</div>}
     </>
   );
 }
