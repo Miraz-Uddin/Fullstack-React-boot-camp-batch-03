5e8973e3398c2f3399a660527b5107b849e0fa62
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-08-06 22:01:46 +0600

    Project First Draft done

--- Tasks/Day-16/src/components/Home.jsx
+++ Tasks/Day-16/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import GeneralInput from "../formInputs/GeneralInput";
 import errorUpdate from "../utils/errorUpdate";
+import ViewDatas from "./ViewDatas";
 
 const userInitialInfo = {
   firstName: "",
@@ -33,6 +34,7 @@ export default function Home() {
   const [userInfoErrors, setUserInfoErrors] = useState(userInitialnfoErrors);
   const [formSubmitBtnActive, setFormSubmitBtnActive] = useState(false);
   const [isErrorShow, setErrorShow] = useState(null);
+  const [viewShow, setViewShow] = useState(false);
 
   const handleChange = (e) => {
     if (document.activeElement === firstNameRef.current)
@@ -76,9 +78,15 @@ export default function Home() {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
-    console.log("Data Can be Submitted");
-    console.log(userInfo);
+    setViewShow(true);
+  };
+  const resetBtn = (e) => {
+    e.preventDefault();
+    setViewShow(false);
+    setUserInfo(userInitialInfo);
+    setUserInfoErrors(userInitialnfoErrors);
+    setFormSubmitBtnActive(false);
+    setErrorShow(null);
   };
 
   const { firstName, lastName, username, email, contact, password, portfolio } =
@@ -108,124 +116,128 @@ export default function Home() {
 
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <div className="card shadow">
-              <div className="card-body">
-                <div className="p-5 text-center">
-                  <h2 style={{ fontFamily: "'Lobster', cursive" }}>
-                    Controlled Form Validations
-                  </h2>
+      {viewShow ? (
+        <ViewDatas userInfo={userInfo} resetBtn={resetBtn} />
+      ) : (
+        <div className="container">
+          <div className="row pt-5">
+            <div className="col-12">
+              <div className="card shadow">
+                <div className="card-body">
+                  <div className="p-5 text-center">
+                    <h2 style={{ fontFamily: "'Lobster', cursive" }}>
+                      Controlled Form Validations
+                    </h2>
+                  </div>
+                  <form className="row g-3" onSubmit={handleSubmit}>
+                    <div className="col-md-6 col-lg-4">
+                      <GeneralInput
+                        label="First Name"
+                        reference={firstNameRef}
+                        type="text"
+                        name="firstName"
+                        value={firstName}
+                        func={handleChange}
+                        errorVal={firstNameError}
+                        isErrorShow={isErrorShow}
+                        setErrorShow={setErrorShow}
+                      />
+                    </div>
+                    <div className="col-md-6 col-lg-4">
+                      <GeneralInput
+                        label="Last Name"
+                        reference={lastNameRef}
+                        type="text"
+                        name="lastName"
+                        value={lastName}
+                        func={handleChange}
+                        errorVal={lastNameError}
+                        isErrorShow={isErrorShow}
+                        setErrorShow={setErrorShow}
+                      />
+                    </div>
+                    <div className="col-md-6 col-lg-4">
+                      <GeneralInput
+                        label="Username"
+                        reference={usernameRef}
+                        type="text"
+                        name="username"
+                        value={username}
+                        func={handleChange}
+                        errorVal={usernameError}
+                        isErrorShow={isErrorShow}
+                        setErrorShow={setErrorShow}
+                      />
+                    </div>
+                    <div className="col-md-6 col-lg-4">
+                      <GeneralInput
+                        label="Email ID"
+                        reference={emailRef}
+                        type="text"
+                        name="email"
+                        value={email}
+                        func={handleChange}
+                        errorVal={emailError}
+                        isErrorShow={isErrorShow}
+                        setErrorShow={setErrorShow}
+                      />
+                    </div>
+                    <div className="col-md-6 col-lg-4">
+                      <GeneralInput
+                        label="Contact Number"
+                        reference={contactRef}
+                        type="text"
+                        name="contact"
+                        value={contact}
+                        func={handleChange}
+                        errorVal={contactError}
+                        isErrorShow={isErrorShow}
+                        setErrorShow={setErrorShow}
+                      />
+                    </div>
+                    <div className="col-md-6 col-lg-4">
+                      <GeneralInput
+                        label="Password"
+                        reference={passwordRef}
+                        type="text"
+                        name="password"
+                        value={password}
+                        func={handleChange}
+                        errorVal={passwordError}
+                        isErrorShow={isErrorShow}
+                        setErrorShow={setErrorShow}
+                      />
+                    </div>
+                    <div className="col-12">
+                      <GeneralInput
+                        label="Portfolio"
+                        reference={portfolioRef}
+                        type="text"
+                        name="portfolio"
+                        value={portfolio}
+                        func={handleChange}
+                        errorVal={portfolioError}
+                        isErrorShow={isErrorShow}
+                        setErrorShow={setErrorShow}
+                      />
+                    </div>
+                    <div className="col-12 d-flex justify-content-center">
+                      <button
+                        type="submit"
+                        className={`btn btn-success btn-lg ${
+                          !formSubmitBtnActive ? "disabled-button" : ""
+                        }`}
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </form>
                 </div>
-                <form className="row g-3" onSubmit={handleSubmit}>
-                  <div className="col-md-6 col-lg-4">
-                    <GeneralInput
-                      label="First Name"
-                      reference={firstNameRef}
-                      type="text"
-                      name="firstName"
-                      value={firstName}
-                      func={handleChange}
-                      errorVal={firstNameError}
-                      isErrorShow={isErrorShow}
-                      setErrorShow={setErrorShow}
-                    />
-                  </div>
-                  <div className="col-md-6 col-lg-4">
-                    <GeneralInput
-                      label="Last Name"
-                      reference={lastNameRef}
-                      type="text"
-                      name="lastName"
-                      value={lastName}
-                      func={handleChange}
-                      errorVal={lastNameError}
-                      isErrorShow={isErrorShow}
-                      setErrorShow={setErrorShow}
-                    />
-                  </div>
-                  <div className="col-md-6 col-lg-4">
-                    <GeneralInput
-                      label="Username"
-                      reference={usernameRef}
-                      type="text"
-                      name="username"
-                      value={username}
-                      func={handleChange}
-                      errorVal={usernameError}
-                      isErrorShow={isErrorShow}
-                      setErrorShow={setErrorShow}
-                    />
-                  </div>
-                  <div className="col-md-6 col-lg-4">
-                    <GeneralInput
-                      label="Email ID"
-                      reference={emailRef}
-                      type="text"
-                      name="email"
-                      value={email}
-                      func={handleChange}
-                      errorVal={emailError}
-                      isErrorShow={isErrorShow}
-                      setErrorShow={setErrorShow}
-                    />
-                  </div>
-                  <div className="col-md-6 col-lg-4">
-                    <GeneralInput
-                      label="Contact Number"
-                      reference={contactRef}
-                      type="text"
-                      name="contact"
-                      value={contact}
-                      func={handleChange}
-                      errorVal={contactError}
-                      isErrorShow={isErrorShow}
-                      setErrorShow={setErrorShow}
-                    />
-                  </div>
-                  <div className="col-md-6 col-lg-4">
-                    <GeneralInput
-                      label="Password"
-                      reference={passwordRef}
-                      type="text"
-                      name="password"
-                      value={password}
-                      func={handleChange}
-                      errorVal={passwordError}
-                      isErrorShow={isErrorShow}
-                      setErrorShow={setErrorShow}
-                    />
-                  </div>
-                  <div className="col-12">
-                    <GeneralInput
-                      label="Portfolio"
-                      reference={portfolioRef}
-                      type="text"
-                      name="portfolio"
-                      value={portfolio}
-                      func={handleChange}
-                      errorVal={portfolioError}
-                      isErrorShow={isErrorShow}
-                      setErrorShow={setErrorShow}
-                    />
-                  </div>
-                  <div className="col-12 d-flex justify-content-center">
-                    <button
-                      type="submit"
-                      className={`btn btn-success btn-lg ${
-                        !formSubmitBtnActive ? "disabled-button" : ""
-                      }`}
-                    >
-                      Submit
-                    </button>
-                  </div>
-                </form>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
