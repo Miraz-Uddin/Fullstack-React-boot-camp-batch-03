3480c6006952591de3b0cc41323285047f7dfc80
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-08-08 00:22:40 +0600

    Blogs Section Converted

--- Tasks/Day-19/src/sections/Blog.jsx
+++ Tasks/Day-19/src/sections/Blog.jsx
@@ -1,212 +1,219 @@
 import React from "react";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 export default function Blog() {
   return (
     <section className="blog">
-      <div className="full-wrapper blog-full">
+      <div className="blog-full">
         <div className="blogBackgroundImageLeft">
           <img
             src="assets/images/blog/bg-left.png"
             alt="blog background image left"
           />
         </div>
-        <div className="wrapper">
-          <div className="blog-heading section_heading">
-            <h2>Latest News</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-          </div>
-          <div className="blog-section">
-            <div className="blog-section-slide">
-              <div className="blog-section-slide-left">
-                <img
-                  src="assets/images/blog/blogImage1.jpg"
-                  alt="blog image 1"
-                />
-                <div className="blog-section-slide-left-overlay">
-                  <div>
-                    <p>12 Aug</p>
-                    <h6>2018</h6>
+        <Container>
+          <Row>
+            <Col>
+              <div className="blog-heading section_heading">
+                <h2>Latest News</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </div>
+              <div className="blog-section">
+                <div className="blog-section-slide">
+                  <div className="blog-section-slide-left">
+                    <img
+                      src="assets/images/blog/blogImage1.jpg"
+                      alt="blog image 1"
+                    />
+                    <div className="blog-section-slide-left-overlay">
+                      <div>
+                        <p>12 Aug</p>
+                        <h6>2018</h6>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="blog-section-slide-right">
+                    <div className="blogPost">
+                      <h2>How to create cool paerns in image process?</h2>
+                      <div className="blog_author">
+                        <div className="blog_author_image">
+                          <img
+                            src="assets/images/blog/blog_Author_image1.jpg"
+                            alt="Author Image 1"
+                          />
+                        </div>
+                        <div className="blog_author_details">
+                          <span>Author</span>
+                          <p>Shahin alom</p>
+                        </div>
+                      </div>
+                      <p>
+                        Lorem ipsum dolor sit amet, conser adipisicing elit, sed
+                        do eiusmod por incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim.
+                      </p>
+                      <a href="#">Read more</a>
+                    </div>
+                    <div className="prev_post">
+                      <div className="prev_post_left">
+                        <p>Prev. Post</p>
+                      </div>
+                      <div className="prev_post_right">
+                        <p>How to create cool in image process?</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="blog-section-slide">
+                  <div className="blog-section-slide-left">
+                    <img
+                      src="assets/images/blog/blogImage2.jpg"
+                      alt="blog image 2"
+                    />
+                    <div className="blog-section-slide-left-overlay">
+                      <div>
+                        <p>26 Dec</p>
+                        <h6>2018</h6>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="blog-section-slide-right">
+                    <div className="blogPost">
+                      <h2>How to create cool paerns in image process?</h2>
+                      <div className="blog_author">
+                        <div className="blog_author_image">
+                          <img
+                            src="assets/images/blog/blog_Author_image2.jpg"
+                            alt="Author Image 2"
+                          />
+                        </div>
+                        <div className="blog_author_details">
+                          <span>Author</span>
+                          <p>ABS Uzzal</p>
+                        </div>
+                      </div>
+                      <p>
+                        Lorem ipsum dolor sit amet, conser adipisicing elit, sed
+                        do eiusmod por incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim.
+                      </p>
+                      <a href="#">Read more</a>
+                    </div>
+                    <div className="prev_post">
+                      <div className="prev_post_left">
+                        <p>Next Post</p>
+                      </div>
+                      <div className="prev_post_right">
+                        <p>How to create cool in image process?</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="blog-section-slide">
+                  <div className="blog-section-slide-left">
+                    <img
+                      src="assets/images/blog/blogImage3.jpg"
+                      alt="blog image 3"
+                    />
+                    <div className="blog-section-slide-left-overlay">
+                      <div>
+                        <p>13 Feb</p>
+                        <h6>2019</h6>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="blog-section-slide-right">
+                    <div className="blogPost">
+                      <h2>How to create hot paerns in image process?</h2>
+                      <div className="blog_author">
+                        <div className="blog_author_image">
+                          <img
+                            src="assets/images/blog/blog_Author_image3.jpg"
+                            alt="Author Image 3"
+                          />
+                        </div>
+                        <div className="blog_author_details">
+                          <span>Author</span>
+                          <p>Tamim asif</p>
+                        </div>
+                      </div>
+                      <p>
+                        Labore et dolore magna aliqua. Ut enim ad minim, lorem
+                        ipsum dolor sit amet, conser adipisicing elit, sed do
+                        eiusmod por incididunt ut.
+                      </p>
+                      <a href="#">Read more</a>
+                    </div>
+                    <div className="prev_post">
+                      <div className="prev_post_left">
+                        <p>Prev. Post</p>
+                      </div>
+                      <div className="prev_post_right">
+                        <p>How to create cool in image process?</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="blog-section-slide">
+                  <div className="blog-section-slide-left">
+                    <img
+                      src="assets/images/blog/blogImage4.jpg"
+                      alt="blog image 4"
+                    />
+                    <div className="blog-section-slide-left-overlay">
+                      <div>
+                        <p>14 Oct</p>
+                        <h6>2019</h6>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="blog-section-slide-right">
+                    <div className="blogPost">
+                      <h2>How to create cool paerns in image process?</h2>
+                      <div className="blog_author">
+                        <div className="blog_author_image">
+                          <img
+                            src="assets/images/blog/blog_Author_image4.jpg"
+                            alt="Author Image 4"
+                          />
+                        </div>
+                        <div className="blog_author_details">
+                          <span>Author</span>
+                          <p>Miraz Uddin</p>
+                        </div>
+                      </div>
+                      <p>
+                        Sed do eiusmod por incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim, lorem ipsum dolor sit amet,
+                        conser adipisicing elit.
+                      </p>
+                      <a href="#">Read more</a>
+                    </div>
+                    <div className="prev_post">
+                      <div className="prev_post_left">
+                        <p>Next Post</p>
+                      </div>
+                      <div className="prev_post_right">
+                        <p>How to create cool in image process?</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="blog-section-slide-right">
-                <div className="blogPost">
-                  <h2>How to create cool paerns in image process?</h2>
-                  <div className="blog_author">
-                    <div className="blog_author_image">
-                      <img
-                        src="assets/images/blog/blog_Author_image1.jpg"
-                        alt="Author Image 1"
-                      />
-                    </div>
-                    <div className="blog_author_details">
-                      <span>Author</span>
-                      <p>Shahin alom</p>
-                    </div>
-                  </div>
-                  <p>
-                    Lorem ipsum dolor sit amet, conser adipisicing elit, sed do
-                    eiusmod por incididunt ut labore et dolore magna aliqua. Ut
-                    enim ad minim.
-                  </p>
-                  <a href="#">Read more</a>
-                </div>
-                <div className="prev_post">
-                  <div className="prev_post_left">
-                    <p>Prev. Post</p>
-                  </div>
-                  <div className="prev_post_right">
-                    <p>How to create cool in image process?</p>
-                  </div>
-                </div>
+              <div className="blog-navigation">
+                <p>
+                  <i className="fa fa-long-arrow-left blog-navigation-left"></i>
+                  <i className="fa fa-long-arrow-right blog-navigation-right"></i>
+                </p>
               </div>
-            </div>
-            <div className="blog-section-slide">
-              <div className="blog-section-slide-left">
-                <img
-                  src="assets/images/blog/blogImage2.jpg"
-                  alt="blog image 2"
-                />
-                <div className="blog-section-slide-left-overlay">
-                  <div>
-                    <p>26 Dec</p>
-                    <h6>2018</h6>
-                  </div>
-                </div>
-              </div>
-              <div className="blog-section-slide-right">
-                <div className="blogPost">
-                  <h2>How to create cool paerns in image process?</h2>
-                  <div className="blog_author">
-                    <div className="blog_author_image">
-                      <img
-                        src="assets/images/blog/blog_Author_image2.jpg"
-                        alt="Author Image 2"
-                      />
-                    </div>
-                    <div className="blog_author_details">
-                      <span>Author</span>
-                      <p>ABS Uzzal</p>
-                    </div>
-                  </div>
-                  <p>
-                    Lorem ipsum dolor sit amet, conser adipisicing elit, sed do
-                    eiusmod por incididunt ut labore et dolore magna aliqua. Ut
-                    enim ad minim.
-                  </p>
-                  <a href="#">Read more</a>
-                </div>
-                <div className="prev_post">
-                  <div className="prev_post_left">
-                    <p>Next Post</p>
-                  </div>
-                  <div className="prev_post_right">
-                    <p>How to create cool in image process?</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="blog-section-slide">
-              <div className="blog-section-slide-left">
-                <img
-                  src="assets/images/blog/blogImage3.jpg"
-                  alt="blog image 3"
-                />
-                <div className="blog-section-slide-left-overlay">
-                  <div>
-                    <p>13 Feb</p>
-                    <h6>2019</h6>
-                  </div>
-                </div>
-              </div>
-              <div className="blog-section-slide-right">
-                <div className="blogPost">
-                  <h2>How to create hot paerns in image process?</h2>
-                  <div className="blog_author">
-                    <div className="blog_author_image">
-                      <img
-                        src="assets/images/blog/blog_Author_image3.jpg"
-                        alt="Author Image 3"
-                      />
-                    </div>
-                    <div className="blog_author_details">
-                      <span>Author</span>
-                      <p>Tamim asif</p>
-                    </div>
-                  </div>
-                  <p>
-                    Labore et dolore magna aliqua. Ut enim ad minim, lorem ipsum
-                    dolor sit amet, conser adipisicing elit, sed do eiusmod por
-                    incididunt ut.
-                  </p>
-                  <a href="#">Read more</a>
-                </div>
-                <div className="prev_post">
-                  <div className="prev_post_left">
-                    <p>Prev. Post</p>
-                  </div>
-                  <div className="prev_post_right">
-                    <p>How to create cool in image process?</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="blog-section-slide">
-              <div className="blog-section-slide-left">
-                <img
-                  src="assets/images/blog/blogImage4.jpg"
-                  alt="blog image 4"
-                />
-                <div className="blog-section-slide-left-overlay">
-                  <div>
-                    <p>14 Oct</p>
-                    <h6>2019</h6>
-                  </div>
-                </div>
-              </div>
-              <div className="blog-section-slide-right">
-                <div className="blogPost">
-                  <h2>How to create cool paerns in image process?</h2>
-                  <div className="blog_author">
-                    <div className="blog_author_image">
-                      <img
-                        src="assets/images/blog/blog_Author_image4.jpg"
-                        alt="Author Image 4"
-                      />
-                    </div>
-                    <div className="blog_author_details">
-                      <span>Author</span>
-                      <p>Miraz Uddin</p>
-                    </div>
-                  </div>
-                  <p>
-                    Sed do eiusmod por incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim, lorem ipsum dolor sit amet, conser
-                    adipisicing elit.
-                  </p>
-                  <a href="#">Read more</a>
-                </div>
-                <div className="prev_post">
-                  <div className="prev_post_left">
-                    <p>Next Post</p>
-                  </div>
-                  <div className="prev_post_right">
-                    <p>How to create cool in image process?</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="blog-navigation">
-            <p>
-              <i className="fa fa-long-arrow-left blog-navigation-left"></i>
-              <i className="fa fa-long-arrow-right blog-navigation-right"></i>
-            </p>
-          </div>
-        </div>
+            </Col>
+          </Row>
+        </Container>
         <div className="blogBackgroundImageRight">
           <img
             src="assets/images/blog/bg-right.png"
