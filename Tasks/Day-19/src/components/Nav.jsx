918505a325b9917c25dda62c694ad048f92e9a0a
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-08-08 00:27:47 +0600

    Navigation Section Converted

--- Tasks/Day-19/src/components/Nav.jsx
+++ Tasks/Day-19/src/components/Nav.jsx
@@ -1,47 +1,52 @@
 import React from "react";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 export default function Nav() {
   return (
     <header>
-      <div className="full-wrapper">
-        <nav>
-          <div className="wrapper">
-            <div className="header-content">
-              <div className="logo">
-                <a href="#">
-                  <img src="assets/images/header/logo.png" alt="Logo Image" />
-                </a>
+      <nav>
+        <Container>
+          <Row>
+            <Col>
+              <div className="header-content">
+                <div className="logo">
+                  <a href="#">
+                    <img src="assets/images/header/logo.png" alt="Logo Image" />
+                  </a>
+                </div>
+                <div className="main-menu">
+                  <ul>
+                    <li>
+                      <a href="#banner" className="activeNavButton">
+                        Home
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#features">Features</a>
+                    </li>
+                    <li>
+                      <a href="#aboutIt">About it</a>
+                    </li>
+                    <li>
+                      <a href="#testimonial">Testimonial</a>
+                    </li>
+                    <li>
+                      <a href="#contact">Contact</a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="search-bar">
+                  <a href="#">
+                    <i className="fa fa-search" aria-hidden="true"></i>
+                  </a>
+                </div>
               </div>
-              <div className="main-menu">
-                <ul>
-                  <li>
-                    <a href="#banner" className="activeNavButton">
-                      Home
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#features">Features</a>
-                  </li>
-                  <li>
-                    <a href="#aboutIt">About it</a>
-                  </li>
-                  <li>
-                    <a href="#testimonial">Testimonial</a>
-                  </li>
-                  <li>
-                    <a href="#contact">Contact</a>
-                  </li>
-                </ul>
-              </div>
-              <div className="search-bar">
-                <a href="#">
-                  <i className="fa fa-search" aria-hidden="true"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-        </nav>
-      </div>
+            </Col>
+          </Row>
+        </Container>
+      </nav>
     </header>
   );
 }
