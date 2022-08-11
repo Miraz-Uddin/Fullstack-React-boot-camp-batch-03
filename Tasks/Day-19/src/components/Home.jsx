991c5ef5aef060c4a9822556206bb8741f72be39
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-08-12 05:33:46 +0600

    Testimonial Section Responsive done

--- Tasks/Day-19/src/components/Home.jsx
+++ Tasks/Day-19/src/components/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import About from "../sections/About/About";
 import Banner from "../sections/Banner/Banner";
-// import Blogs from "../sections/Blogs";
-// import Contact from "../sections/Contact";
+import Blogs from "../sections/Blogs";
+import Contact from "../sections/Contact";
 import Faq from "../sections/Faq/Faq";
 import Features from "../sections/Features/Features";
 import Services from "../sections/Services/Services";
-// import Testimonials from "../sections/Testimonials";
+import Testimonials from "../sections/Testimonials";
 import WatchVideo from "../sections/WatchVideo";
 
 export default function Home() {
@@ -18,10 +18,9 @@ export default function Home() {
       <Faq />
       <WatchVideo />
       <Services />
-      {/* 
       <Testimonials />
       <Blogs />
-      <Contact /> */}
+      <Contact />
     </>
   );
 }
