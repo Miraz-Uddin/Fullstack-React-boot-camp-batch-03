247a025e50f7178dc48e26a849761a40483e0d00
Miraz <freelancer.miraz.uddin@gmail.com>
2022-08-15 19:43:48 +0600

    Funky Staff Added

--- Tasks/Day-26/src/App.jsx
+++ Tasks/Day-26/src/App.jsx
@@ -1,16 +1,30 @@
 import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Nav from "./components/Nav";
 
 function App() {
+  // Funky Promotional
+  console.log(
+    "%cWhat are you doing here?! you sneaky developer...",
+    "color: #b432ff"
+  );
+  console.log(
+    "%cDM me 👉 https://www.facebook.com/miraz.uddin.7773631",
+    "color: #b432ff"
+  );
+  console.log("%c— Miraz Vai", "color: #777777");
+
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <BrowserRouter>
       <Nav />
-      <Home />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
